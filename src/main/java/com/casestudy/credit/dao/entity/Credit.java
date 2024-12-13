ed8c57c7137f20e57567b503f49c58cfbc3a3ada
Kadircan <yalnizkadircan@gmail.com>
2024-12-14 00:56:36 +0300

    feat: add close date field

--- src/main/java/com/casestudy/credit/dao/entity/Credit.java
+++ src/main/java/com/casestudy/credit/dao/entity/Credit.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -14,15 +15,16 @@ import java.util.List;
 @Table(name = "CREDIT")
 public class Credit extends BaseCreatedColumns{
     @Id
-    @SequenceGenerator(name = "SEQ_CREDIT", sequenceName = "SEQ_CREDIT")
+    @SequenceGenerator(name = "SEQ_CREDIT", sequenceName = "SEQ_CREDIT", allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_CREDIT")
     @Column(name = "ID", length = 16)
     private Long id;
     @Column(name = "STATUS", length = 1)
-    //BOOLEAN OLUR MU?
-    private Integer status;
+    private Integer status = 1;
     @Column(name = "REMAINING_AMOUNT", precision = 19, scale = 2)
     private BigDecimal remainingAmount;
+    @Column(name = "CLOSE_DATE")
+    private Date closeDate;
     @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "CUSTOMER_ID", referencedColumnName = "ID")
